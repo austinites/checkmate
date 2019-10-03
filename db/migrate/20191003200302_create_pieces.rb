b037d954d01b6ab499be1255a67bc856118a7d62
hyon lee <hyon.lee@utexas.edu>
2019-10-03 20:18:25 +0000

    model update

---
+++ db/migrate/20191003200302_create_pieces.rb
@@ -0,0 +1,13 @@
+class CreatePieces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pieces do |t|
+      t.integer :xcoordinate
+      t.integer :ycoordinate
+      t.string :piece_type
+      t.string :color
+      t.integer :piece_owner_id
+      t.integer :in_check_player_id
+      t.timestamps
+    end
+  end
+end
