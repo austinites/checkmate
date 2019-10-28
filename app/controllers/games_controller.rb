62f6b31cfe621324b6a5902365f6bdbc9dffde12
Matthew Gips <msg558@gmail.com>
2019-10-28 20:46:39 +0000

    added support for joining a game, updated how games are listed on homepage

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,19 @@
 class GamesController < ApplicationController
-
+before_action :authenticate_user!, only: [:new, :create, :show]
   def show
     @game = Game.find(params[:id])
   end
+
+  def new
+  	@game=current_user.games.create(white_id: current_user.id, turn_number: 1)
+  	@game.initialize_board
+  	redirect_to :action => "show", :id => @game.id
+  end
+
+  def update
+  	@game = Game.find(params[:id])
+  	@game.update_attributes(black_id: current_user.id)
+  	redirect_to :action => "show", :id => @game.id
+  end
+
 end
